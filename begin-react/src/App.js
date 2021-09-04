7e7e0ca9f57602a0b0fd96f56aecdb6af83d28c6
minhee.son <minhee4735@gmail.com>
2021-09-05 08:59:10 +0900

    feat barbelJS 문법 익히기

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,13 +1,31 @@
 import React from "react";
 import Hello from './Hello';
+import './App.css';
 
 function App() {
+  const name = '홍길동';
+  const style = {
+    backgroundColor : 'black',
+    color : 'aqua',
+    fontSize : 24,
+    padding : '1rem'
+  };
+
   return (
-    <div>
-      <Hello />
-      <Hello />
-      <Hello />
-    </div>
+    <>
+      <div className="gray-box">
+        {/* 주석은 이렇게 씁니다. */}
+        <Hello 
+            //여기에서는 주석을 이렇게 써요!
+        />
+        <Hello />
+      </div>
+
+      <br></br>
+      <hr></hr>
+
+      <span style={style}>{name}</span>
+    </>
   );
 }
 
