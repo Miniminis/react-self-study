8b9c54b10a46ee05bf1fb68297f5889c3719dd97
minhee.son <minhee4735@gmail.com>
2021-09-05 08:24:09 +0900

    feat HelloWorld component 생성하기 실습 완료 - component는 두 가지 종류로 만들 수 있다. 1) 함수형 2) 클래스형

---
+++ begin-react/src/Hello.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+//component 
+    // 1. 함수형
+    // 2. 클래스형
+
+function HelloWorld() {
+    return <div>안녕하세요!</div>;
+}
+
+export default HelloWorld;      //HelloWorld component 를 내보낸다. 
