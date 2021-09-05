6c598d0a1162760cb281e6c0cb11ee89104d500c
minhee.son <minhee4735@gmail.com>
2021-09-05 11:05:38 +0900

    feat 여러개의 input 상태 관리하기 - 객체의 상태를 업데이트할때는 객체를 1회 복사 후, 값을 업데이트해주어야 한다.   - 불변성을 지키면서 업데이트해야한다.

--- begin-react/src/InputSample.js
+++ begin-react/src/InputSample.js
@@ -1,23 +1,45 @@
 import React, {useState} from "react";
 
 function InputSample() {
-    const [text, setText] = useState('');
-    
+    const [inputs, setInputs] = useState({
+        name : '',
+        nickname: ''
+    });
+
+    const {name, nickname} = inputs; 
+
     const onChange = (e) => {
-        setText(e.target.value);
+        const {name, value} = e.target;      //여러개의 input key 들 받아서 일괄적으로 객체 업데이트 처리
+
+        setInputs({
+            ...inputs,                      //객체 업데이트 시에는 1회 복사 후, 값 업데이트 진행해야함
+            [name] : value
+        });
     };
 
     const onReset = () => {
-        setText('');
+        setInputs({
+            name : '',
+            nickname : ''
+        });
     }
 
     return (
         <div>
-            <input onChange={onChange} value={text}/>
+            <input 
+                placeholder="이름" 
+                onChange={onChange} 
+                name="name"
+                value={name} />
+            <input 
+                placeholder="닉네임" 
+                onChange={onChange} 
+                name="nickname"
+                value={nickname} />
             <button onClick={onReset}>초기화</button>
             <div>
                 <b>값 : </b>
-                {text}
+                {name} ({nickname})
             </div>
         </div>
     );
