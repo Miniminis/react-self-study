b430acfa6b8f346b1918135217be848f5736c09c
minhee.son <minhee4735@gmail.com>
2021-09-05 17:15:25 +0900

    feat 배열에 새로운 항목 추가하기 - 방법1) 스프레드 연산자 이용하기 - 방법2) concat() 함수 이용하기 - 주의) push() 는 동작하지 않는다.

--- begin-react/src/App.js
+++ begin-react/src/App.js
@@ -1,8 +1,24 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
+import CreateUser from "./CreateUser";
 import UserList from "./UserList";
 
 function App() {
-  const users = [
+  const [inputs, setInputs] = useState({
+    username : '',
+    email : '' 
+  });
+
+  const { username, email } = inputs;
+
+  const onChange = (e) => {
+    const {name, value} = e.target;
+    setInputs({
+      ...inputs,
+      [name]: value
+    });
+  };
+
+  const [users, setUsers] = useState([
       {
           id: 1,
           username : 'flash',
@@ -18,19 +34,38 @@ function App() {
           username : 'black',
           email: 'black@gmail.com'
       }
-  ];
+  ]);
 
   const nextId = useRef(4);   //굳이 리렌더링할 필요없는 값
   
   const onCreate = () => {
-    console.log(nextId.current);
+    const user = {
+      id : nextId.current,
+      username,
+      email
+    };
+
+    //users.push(user);     //기존배열에 push() 하여 요소를 단순히 추가하는 방식으로는 동작하지 않는다(업데이트 되지 않음). 반드시 기존 배열을 복사한 뒤, 새로운 요소를 추가한 새로운 배열을 만들어주어야 한다. 
+    // setUsers([...users, user]);   //방법1. 기존배열을 스프레드 연산자로 복사한 뒤, 새로운 user 객체 추가한 새로운 배열 만들기
+    setUsers(users.concat(user));    //방법2. concat 연산자를 사용하여 기존 배열 + 새로운 요소 = 새로운 배열 만들기
+    
+    setInputs({
+      username: '',
+      email: ''
+    });
+
     nextId.current += 1;
-  }
+  };
 
   return (
-    <div onClick={onCreate()}>
-      <UserList users={users}/>
-    </div>
+      <>
+        <CreateUser 
+          username={username}
+          email={email}
+          onChange={onChange}
+          onCreate={onCreate} />
+        <UserList users={users}/>
+      </>
   );
 }
 
