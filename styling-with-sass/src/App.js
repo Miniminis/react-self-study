783d379511e2f2be641c08c255a2ab5552a5bf38
minhee.son <minhee4735@gmail.com>
2021-09-13 07:42:09 +0900

    feat SCSS 사용법 및 재사용가능한 컴포넌트 만들기 실습 (버튼)

--- styling-with-sass/src/App.js
+++ styling-with-sass/src/App.js
@@ -5,7 +5,29 @@ function App() {
   return (
     <div className="App">
       <div className="buttons">
+        <Button size='large' onClick={() => console.log('클릭됐어요!')}>BUTTON</Button>
         <Button>BUTTON</Button>
+        <Button size='small'>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size='large' color='pink'>BUTTON</Button>
+        <Button color='pink'>BUTTON</Button>
+        <Button size='small' color='pink'>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size='large' color='gray'>BUTTON</Button>
+        <Button color='gray'>BUTTON</Button>
+        <Button size='small' color='gray'>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size='large' color='gray' outline='true'>BUTTON</Button>
+        <Button color='blue' outline='true'>BUTTON</Button>
+        <Button size='small' color='pink' outline='true'>BUTTON</Button>
+      </div>
+      <div className="buttons">
+        <Button size='large' color='gray' fullWidth='true'>BUTTON</Button>
+        <Button color='blue' fullWidth='true'>BUTTON</Button>
+        <Button size='small' color='pink' fullWidth='true'>BUTTON</Button>
       </div>
     </div>
   );
