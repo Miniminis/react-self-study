7d8fe82ef4c3e337af33e6142410cfbd63520435
minhee.son <minhee4735@gmail.com>
2021-09-13 22:28:04 +0900

    feat styled-components lib 사용법 익히기

--- styling-with-styled-components/src/components/Button.js
+++ styling-with-styled-components/src/components/Button.js
@@ -1,5 +1,6 @@
 import React from "react";
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { darken, lighten } from 'polished';
 
 const StyledButton = styled.button`
   /* 공통 스타일 */
@@ -18,13 +19,18 @@ const StyledButton = styled.button`
   font-size: 1rem;
 
   /* 색상 */
-  background: #228be6;
-  &:hover {
-    background: #339af0;
-  }
-  &:active {
-    background: #1c7ed6;
-  }
+  ${({ theme, color }) => {
+      const selected = theme.palette[color];
+      return css`
+          background: ${selected};
+          &:hover {
+            background: ${lighten(0.1, selected)};
+          }
+          &:active {
+            background: ${darken(0.1, selected)};
+          }
+      `;
+  }};
 
   /* 기타 */
   & + & {
@@ -33,8 +39,12 @@ const StyledButton = styled.button`
 `;
 
 
-function Button({ children, ...rest }) {
-    return <StyledButton {...rest}>{ children }</StyledButton>
+function Button({ children, color, ...rest }) {
+    return <StyledButton color={color} {...rest}>{ children }</StyledButton>
+}
+
+Button.defaultProps ={
+    color: 'blue'
 }
 
 export default Button;
